d3b7da0c397c2c1c31887cb8741bed8ad05b2c5f
Vladimir Rybalchenko <tazik.ne@gmail.com>
2021-06-04 10:31:55 +0300

    added lists

--- main_.py
+++ main_.py
@@ -1,4 +1,16 @@
 with open('recipes.txt') as file:
+    list_0 = []
+    list_1 = []
+    list_2 = []
     for line in file:
         if '|' in line:
-            print(line.strip())
+            list_1.append(line.strip().split('|'))
+        else:
+            if len(line) > 1:
+                list_0.append(line.strip())
+            else:
+                pass
+        
+    print(list_0)
+    print(list_1)
+    print(list_2)
