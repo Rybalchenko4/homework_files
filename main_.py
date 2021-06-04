55b5a65ae464cf08fbc000c0b21eb2cc8e7ce60a
Vladimir Rybalchenko <tazik.ne@gmail.com>
2021-06-04 16:50:33 +0300

    updated .gitignore

--- main_.py
+++ main_.py
@@ -1,16 +1,19 @@
 with open('recipes.txt') as file:
+        
     list_0 = []
     list_1 = []
-    list_2 = []
     for line in file:
         if '|' in line:
-            list_1.append(line.strip().split('|'))
+            list_1.append(line.strip().split('|')) #ингридиент, количество, еденица измерения
         else:
             if len(line) > 1:
                 list_0.append(line.strip())
             else:
                 pass
-        
-    print(list_0)
-    print(list_1)
-    print(list_2)
+
+    list_2 = list_0[::2] #название блюда
+    list_3 = list_0[1::2] #количество ингридиентов
+
+print(list_1)
+print(list_2)
+print(list_3)
